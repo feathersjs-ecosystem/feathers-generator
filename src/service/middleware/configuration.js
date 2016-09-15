d2f5352007d06bdcd65224147376c6a9023c1a39
Kyle Campbell <mail@slajax.com>
2016-09-15 14:57:11 -0700

    service generator bootstrapping

--- src/service/middleware/configuration.js
+++ src/service/middleware/configuration.js
@@ -1,4 +1,6 @@
-import Debug from 'debug'
+import fs from 'fs';
+import path from 'path';
+import Debug from 'debug';
 import merge from 'lodash.merge';
 
 const debug = Debug('feathers-generator:configuration');
@@ -9,38 +11,31 @@ const debug = Debug('feathers-generator:configuration');
 // - write out updated config files to this.options.root/config
 
 export default function(options) {
+
   return function configuration(files, metalsmith, done){
+
     const meta = metalsmith.metadata();
-    //const { model } = meta.answers;
-    const existing = meta.default;
-    let template = files['config/default.json'];
+    const answers = meta.answers;
 
-    if (template && template.contents) {
-      try {
-        template = JSON.parse(template.contents.toString());
+    // @slajax refactor this into src/util/configuration so we have getter/setters for async changing of all configs
+    // load existing root config
+    const feathersConfigPath = path.resolve(options.root, 'server/feathers.json');
+    const existingFeathersConfig = require(feathersConfigPath);
+
+    // add new service to root config for bootstrapping
+    const serviceConfigPath = path.join('./server/services', options.name, options.name+'.json.js' );
+    const feathersConfigChanges = { use: { ['/'+options.name]: serviceConfigPath } };
+    const newFeathersConfig = merge(feathersConfigChanges, existingFeathersConfig);
+
+    // write out new root config so service is bootstrapped (respect white space)
+    fs.writeFile(feathersConfigPath, JSON.stringify(newFeathethersConfigrsConfig, null, 2), function(err) {
+      for(var k in files) {
+        files[k.replace('service', options.name)] = files[k];
+        delete files[k]
       }
-      catch(error) {
-        return done(error);
-      }
-    }
-    else {
-      template = {};
-    }
+      done(err)
 
-    //if (model) {
-      //template.model = model;
-    //}
-    //
+    });
 
-    for(var k in files) {
-      files[k.replace('service', options.name)] = files[k];
-      delete files[k]
-    }
-
-    const newJSON = merge(template, existing);
-
-    files[options.name+'.json.js'].contents = JSON.stringify(newJSON, null, 2);
-
-    done();
   };
 };
