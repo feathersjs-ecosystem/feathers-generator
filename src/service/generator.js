ebdda8d70abfa6b6c08df0d23e298835116cb0ef
Kyle Campbell <mail@slajax.com>
2016-09-19 11:59:45 -0700

    service generator mount refactor

--- src/service/generator.js
+++ src/service/generator.js
@@ -4,7 +4,7 @@ const debug = require('debug')('feathers-generator:service');
 // Metalsmith + middleware
 const Metalsmith = require('metalsmith');
 const copy = require('metalsmith-copy');
-const configuration = require('./middleware/configuration')
+const mount = require('./middleware/mount')
 
 const json = require('../utils/json');
 const ask = require('../utils/ask');
@@ -14,22 +14,22 @@ const TEMPLATE_PATH = path.resolve(__dirname, 'templates');
 module.exports = function(prompt, done, options) {
 
   const metalsmith = Metalsmith(TEMPLATE_PATH);
-  const SERVICE_PATH = path.resolve(options.root, 'server/services', options.name);
+  const SERVICE_PATH = path.resolve(options.path, options.name);
 
   metalsmith
     .metadata({ options })
     // Read in any existing config files and attach to metadata
     // TODO slajax or EK refactor option args into util fn so not duplicated
     .use(json({
-      meta: path.resolve(__dirname, 'meta.json'),
+      //meta: path.resolve(__dirname, 'meta.json'),
       default: path.join(options.root, 'config', 'default.json'),
       staging: path.join(options.root, 'config', 'staging.json'),
       production: path.join(options.root, 'config', 'production.json'),
       pkg: path.join(options.root, 'package.json'),
-      feathers: path.join(options.root, 'server', 'feathers.json')
+      feathers: path.join(options.path, 'feathers.json')
     }))
-    .use(ask({ callback: prompt }))
-    .use(configuration(options))
+    //.use(ask({ callback: prompt }))
+    .use(mount(options))
     .source(TEMPLATE_PATH)
     .destination(SERVICE_PATH)
     .build(function(error){
@@ -37,7 +37,7 @@ module.exports = function(prompt, done, options) {
         return done(error)
       }
 
-      const message = `Successfully generated ${options.template} "${options.name} at ${SERVICE_PATH}"`;
+      const message = `Successfully generated ${options.template} "${options.name}" at ${SERVICE_PATH}`;
       debug(message);
       done(null, message);
     })
--- src/service/middleware/mount.js
+++ src/service/middleware/mount.js
@@ -3,37 +3,39 @@ import path from 'path';
 import Debug from 'debug';
 import merge from 'lodash.merge';
 
-const debug = Debug('feathers-generator:configuration');
+const debug = Debug('feathers-generator:mount');
 
-// TODO (EK): Handle config files
-// - plug in this.options in to template/config files
-// - Merge this.config with rendered config template
-// - write out updated config files to this.options.root/config
+// Add to use property on feathers.json if --mount defined
 
 export default function(options) {
 
-  return function configuration(files, metalsmith, done){
+  return function mount(files, metalsmith, done){
+
+    // if not mounting, skip
+    if(!options.mount) {
+      return done(null);
+    }
 
     const meta = metalsmith.metadata();
     const answers = meta.answers;
 
     // @slajax refactor this into src/util/configuration so we have getter/setters for async changing of all configs
     // load existing root config
-    const feathersConfigPath = path.resolve(options.root, 'server/feathers.json');
+    const feathersConfigPath = path.resolve(options.mount);
     const existingFeathersConfig = require(feathersConfigPath);
 
     // add new service to root config for bootstrapping
-    const serviceConfigPath = path.join('./server/services', options.name, options.name+'.json.js' );
+    const serviceConfigPath = path.resolve(options.path, options.name, options.name+'.json.js' );
     const feathersConfigChanges = { use: { ['/'+options.name]: serviceConfigPath } };
     const newFeathersConfig = merge(feathersConfigChanges, existingFeathersConfig);
 
     // write out new root config so service is bootstrapped (respect white space)
-    fs.writeFile(feathersConfigPath, JSON.stringify(newFeathethersConfigrsConfig, null, 2), function(err) {
+    fs.writeFile(feathersConfigPath, JSON.stringify(newFeathersConfig, null, 2), function(err) {
       for(var k in files) {
         files[k.replace('service', options.name)] = files[k];
         delete files[k]
       }
-      done(err)
+      done(err);
 
     });
 
