67178f091b55fcb8cd69be25d20993805e191f8a
Kyle Campbell <kc@kc.io>
2016-10-24 02:30:48 -0700

    semistandard fix

--- src/service/middleware/render.js
+++ src/service/middleware/render.js
@@ -1,17 +1,16 @@
 var async = require('async');
 var render = require('consolidate').handlebars.render;
 
-module.exports = function(/*options*/) {
-
-  return function(files, metalsmith, done){
+module.exports = function (/* options */) {
+  return function (files, metalsmith, done) {
     var keys = Object.keys(files);
     var metadata = metalsmith.metadata();
 
     async.each(keys, run, done);
 
-    function run(file, done){
+    function run (file, done) {
       var str = files[file].contents.toString();
-      render(str, metadata, function(err, res){
+      render(str, metadata, function (err, res) {
         if (err) {
           return done(err);
         }
