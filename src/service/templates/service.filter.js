8337e63d85a9e7afcabf67aa337176eb3f2ae79b
Kyle Campbell <kc@kc.io>
2016-11-12 13:01:15 -0800

    default hooks + filters

--- src/service/templates/service.filter.js
+++ src/service/templates/service.filter.js
@@ -1,9 +1,13 @@
 module.exports = function() {
   if(process.env.NODE_ENV !== 'production') {
-    console.warn(`You are using the default filter for the \`{{options.name}}\` service (in ${__dirname}). This means all clients will get every real-time event. For more information how to filter events see http://docs.feathersjs.com/real-time/filtering.html`);
+    console.warn(`You are using the default filter for the \`{{options.name}}\` service (in ${__dirname}).`);
+    console.warn(`This means all clients will get every real-time event.`);
+    console.warn(`For more information how to filter events see http://docs.feathersjs.com/real-time/filtering.html`);
+    console.warn(``);
   }
 
   return function() {
+    console.log(`{{options.name}} filter returned: true`);
     return true;
   };
 };
