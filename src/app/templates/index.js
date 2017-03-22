9e1b42305fb23c7839f84e871cae4631ce75c8d8
Kyle Campbell <kc@kc.io>
2017-03-22 14:20:55 -0700

    v7.x.x doesn't work without these changes

--- src/app/templates/index.js
+++ src/app/templates/index.js
@@ -4,4 +4,4 @@ const app = require('./server/app');
 
 app.start().then(() =>
   app.info(`Application listening on port ${app.config.get('port')}`)
-).catch(e => app.error(e));
+).catch(e => console.error(e));
