8726f78dc2cb75c6c4475cf6ed8248811d0c6f75
Kyle Campbell <kc@kc.io>
2017-03-23 15:34:44 -0700

    fix #48 and leverage installer for deps

--- src/utils/install.js
+++ src/utils/install.js
@@ -8,23 +8,24 @@ import { spawn } from 'child_process';
 
 const debug = Debug('feathers-generator:install');
 
-export default function (options) {
+export default function (options, deps = []) {
   return new Promise((resolve, reject) => {
     let packagePath = path.resolve(options.root, 'package.json');
     let packageJSON = require(packagePath);
     let engine = packageJSON.engines.yarn ? 'yarn' : 'npm';
+    let action = deps.length && engine === 'yarn' ? 'add' : 'install';
 
     // yarn fall back is in src/app/middleware/package-json.js:68
     // yarn fall back is in src/repo/middleware/package-json.js:60
 
     console.log();
-    console.log(`Installing dependencies using ${engine}...`);
+    console.log(`${engine} ${action} ${deps.join(' ')}`);
     console.log();
 
-    const installer = spawn(engine, ['install'], {stdio: 'inherit', cwd: options.root});
+    const installer = spawn(engine, [action, ...deps], {stdio: 'inherit', cwd: options.root});
 
     installer.on('close', function (code) {
-      debug(`'${engine} install' exited with code ${code}`);
+      debug(`'${engine} ${action} ${deps.join(' ')}' exited with code ${code}`);
 
       if (code === 0) {
         return resolve();
