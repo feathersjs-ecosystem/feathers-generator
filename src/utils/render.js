5d17159f8fd6461c2273610fed8c363a2aa2cd10
Kyle Campbell <kc@kc.io>
2017-05-02 20:03:30 -0700

    debug render

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,5 +1,6 @@
 var async = require('async');
 var render = require('consolidate').handlebars.render;
+var debug = require('debug')('feathers-generator:render')
 
 module.exports = function (/* options */) {
   return function (files, metalsmith, done) {
@@ -14,7 +15,7 @@ module.exports = function (/* options */) {
       var str = files[file].contents.toString();
       render(str, metadata, function (err, res) {
         if (err) {
-          console.log(file);
+          debug('could not render file', {file: file});
           return done(err);
         }
         files[file].contents = new Buffer(res);
